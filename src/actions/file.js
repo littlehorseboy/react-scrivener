494bfa824ffad03f906ac945243a9308dad1da3f
littlehorseboy <littlehorseboy@gmail.com>
2019-04-18 23:25:41 +0800

    feat: rxjs/ajax 改 axios

--- src/actions/file.js
+++ src/actions/file.js
@@ -1,6 +1,5 @@
-// import Axios from 'axios';
-import { ajax } from 'rxjs/ajax';
-import { delay, map, mergeMap } from 'rxjs/operators';
+import Axios from 'axios';
+import { delay, mergeMap } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 
 export const FETCH_FILE = 'FETCH_FILE';
@@ -20,9 +19,11 @@ export const fileEpic = action$ => action$.pipe(
   ofType(FETCH_FILE),
   delay(1000),
   mergeMap(
-    action => ajax
-      .getJSON(`https://api.github.com/users/${action.payload}`).pipe(
-        map(response => fetchFileFulfilled(response)),
-      ),
+    action => Axios({
+      method: 'get',
+      url: `https://api.github.com/users/${action.payload}`,
+    })
+      .then(response => fetchFileFulfilled(response.data))
+      .catch(error => console.error(error)),
   ),
 );
